feb971fa09ed0e5bdf03ddb25e8bae5086382a72
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 21:42:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,9 +90,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holidays|
-    holiday.each do |holiday, list|
+    holidays.each do |holiday, list|
       list << "BBQ"
     end
   end
-  
+
 end
