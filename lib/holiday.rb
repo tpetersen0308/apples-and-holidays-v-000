9783321b77a988bbe8390e1fa43b923d0a6fffa9
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 20:53:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holiday, list|
     list << supply
+  end
 end
 
 
